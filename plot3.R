ab2369c1c87f4a2001b1ef80056acb904b29241b
Tadakimi Tomita <teatimest@gmail.com>
2016-04-12 01:38:46 -0400

    legend fixed

--- plot3.R
+++ plot3.R
@@ -16,7 +16,7 @@ epower.trim$Time <- strptime(paste(epower.trim$Date,
                              "%d/%m/%Y %H:%M:%S")
 
 
-#draw plot3.R
+###draw plot3.R
 plot(epower.trim$Time, epower.trim$Sub_metering_1, 
      type = "l", #line graph
      ylab = "Energy sub metering",
@@ -29,9 +29,11 @@ points(epower.trim$Time, epower.trim$Sub_metering_2,
 #add metering 3 as red line
 points(epower.trim$Time, epower.trim$Sub_metering_3, 
        type = "l", col = "blue")
-legend("topright", 
-       col = c("black", "blue", "red"), 
-       legend = c("May", "Other Months"))
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
+       lty = c(1,1,1), 
+       col = c("black","red","blue"),
+       y.intersp = 0.7,
+       bty = "n") #add legend
 #copy plot to file device and save file as 480x480 png image
 dev.copy(png, file = "plot3.png", width = 480, height = 480)
 dev.off() #close the device
